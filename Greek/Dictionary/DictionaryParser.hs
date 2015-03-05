d93bf42c176bfbc396cfdcf82473562184aede45
Timothy Dees <timothy.dees@gmail.com>
2015-03-04 17:32:06 -0800

    Finished Noun parser

--- Greek/Dictionary/DictionaryParser.hs
+++ Greek/Dictionary/DictionaryParser.hs
@@ -3,15 +3,21 @@
 module Greek.Dictionary.DictionaryParser (
 	) where
 
-import Data.Char
 import qualified Data.Text as T
 import Data.Attoparsec.Text
 import Greek.Dictionary.Types
-import Control.Applicative
+
+dialectParser :: Parser [Dialect]
+dialectParser = do
+	stringVal <- xmlWrapper "dialect"
+	return $ T.words stringVal
+
+featureParser :: Parser Feature
+featureParser = xmlWrapper "feature"
 
 numParser :: Parser NumberGr
 numParser = do
-	word <- xmlWrapperAlpha "number"
+	word <- xmlWrapper "number"
 	return $ case word of
 		"sg" -> Singular
 		"dual" -> Dual
@@ -19,26 +25,46 @@ numParser = do
 		_ -> error $ "unexpected number"
 
 genderParser :: Parser Gender
-genderParser = xmlWrapperAlpha "gender" >>=
-		choice [
-			"masc" *> return Masculine
-		  , "fem"  *> return Feminine
-		  , "neut" *> return Neuter]
+genderParser = do
+	word <- xmlWrapper "gender"
+	return $ case word of
+		"masc" -> Masculine
+		"fem" ->  Feminine
+		"neut" -> Neuter
+		_ -> error "invalid gender."
+
+caseParser :: Parser Case
+caseParser = do
+	word <- xmlWrapper "case"
+	return $ case word of
+		"nom" -> Nominative
+		"acc" -> Accusative
+		"gen" -> Genitive
+		"voc" -> Vocative
+		"dat" -> Dative
+		_ -> error "invalid case."
 
 
---nounParser :: Parser Noun
---nounParser = do
---	string "<pos>noun</pos>"
---	num <- numParser
---	g <- genderParser
+nounParser :: Parser Noun
+nounParser = do
+	string "<pos>noun</pos>"
+	num <- numParser
+	g <- genderParser
+	c <- caseParser
+	d <- option [T.empty] dialectParser
+	f <- option T.empty featureParser
+	return $ Noun num g c d f
 
 
-xmlWrapperAlpha :: T.Text -> Parser T.Text	
-xmlWrapperAlpha st = do
+
+
+
+xmlWrapper :: T.Text -> Parser T.Text	
+xmlWrapper st = do
 	char '<'
 	string st
 	char '>'
-	val <- takeWhile1 isAlpha
+	val <- takeTill (=='<')
 	string "</"
 	string st
 	char '>'
