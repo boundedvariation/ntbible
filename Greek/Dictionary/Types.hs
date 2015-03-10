3fd16f508f85c08999733a31dba2e6c78d66a8c1
Timothy Dees <timothy.dees@gmail.com>
2015-03-09 17:20:02 -0700

    Replaced No instances / built out Database code

--- Greek/Dictionary/Types.hs
+++ Greek/Dictionary/Types.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Greek.Dictionary.Types (
   MorphEntry(..)
@@ -29,38 +31,39 @@ module Greek.Dictionary.Types (
   , Degree(..)
   , Irregular(..)
   , Feature
+  , Key
+  , Value
+  , Database(..)
   ) where
 
+import Data.Typeable
+import Data.SafeCopy
 import qualified Data.Text as T
 import qualified Data.Map as M
 
 data NumberGr = Singular 
         | Plural 
-        | Dual
-        | NoNumber deriving (Eq, Show)
+        | Dual deriving (Eq,Show,Typeable)
 
 data Degree = NoDegree
         | Comparative
-        | Superlative deriving (Eq, Show)
+        | Superlative deriving (Eq,Show,Typeable)
 
 data Gender = Masculine 
       | Feminine 
-      | Neuter
-      | NoGender deriving (Eq, Show)
+      | Neuter deriving (Eq,Show,Typeable)
 
 data Case = Nominative 
       | Genitive 
       | Accusative 
       | Dative 
-      | Vocative
-      | NoCase deriving (Eq, Show)
+      | Vocative deriving (Eq,Show,Typeable)
 
 type Dialect = T.Text
 
 data Person = First 
       | Second 
-      | Third
-      | NoPerson deriving (Eq, Show)
+      | Third deriving (Eq,Show,Typeable)
 
 data Tense = Present 
        | Imperfect 
@@ -68,31 +71,28 @@ data Tense = Present
        | Perfect
        | Future
        | FuturePerfect
-       | Pluperfect
-       | NoTense deriving (Eq, Show)
+       | Pluperfect deriving (Eq,Show,Typeable)
 
 data Mood =  Indicative
        | Imperative
        | Subjunctive
-       | Optative
-       | NoMood deriving (Eq, Show)
+       | Optative deriving (Eq,Show,Typeable)
 
 data Voice = Active
        | Passive
        | Middle
-       | MidPass
-       | NoVoice deriving (Eq, Show)
+       | MidPass deriving (Eq,Show,Typeable)
 
 type Feature = T.Text
 
-type MorphLookup = M.Map T.Text MorphEntry
+type MorphLookup = M.Map T.Text [MorphEntry]
 
 data MorphEntry = MorphEntry {
      lemma      :: T.Text
   ,  wordForm   :: T.Text
   ,  morphology :: Morphology
   ,  dialect    :: [Dialect]
-  ,  feature    :: Feature  } deriving (Eq,Show)
+  ,  feature    :: Feature  } deriving (Eq,Show,Typeable)
 
 data Morphology = MorphNoun Noun 
         | MorphVerb Verb 
@@ -108,77 +108,110 @@ data Morphology = MorphNoun Noun
         | MorphArt  Article
         | MorphNum  Numeral
         | MorphAdvl Adverbial
-        | MorphIrr  Irregular deriving (Eq,Show)
+        | MorphIrr  Irregular deriving (Eq,Show,Typeable)
 
 data Noun = Noun {
-    nounNumber     :: NumberGr
-  , nounGender     :: Gender
-  , nounCase       :: Case
-} deriving (Eq,Show)
+    nounNumber     :: Maybe NumberGr
+  , nounGender     :: Maybe Gender
+  , nounCase       :: Maybe Case
+} deriving (Eq,Show,Typeable)
 
 data Verb = Verb {
-    verbPerson  :: Person
-  , verbNumber  :: NumberGr
-  , verbTense   :: Tense
-  , verbMood    :: Mood
-  , verbVoice   :: Voice
-} deriving (Eq,Show)
+    verbPerson  :: Maybe Person
+  , verbNumber  :: Maybe NumberGr
+  , verbTense   :: Maybe Tense
+  , verbMood    :: Maybe Mood
+  , verbVoice   :: Maybe Voice
+} deriving (Eq,Show,Typeable)
 
 data Preposition = Preposition {
-   prepDegree :: Degree
-} deriving (Eq,Show)
+   prepDegree :: Maybe Degree
+} deriving (Eq,Show,Typeable)
 
-data Exclamation = Exclamation deriving (Eq,Show)
+data Exclamation = Exclamation deriving (Eq,Show,Typeable)
 
 data Participle = Participle {
-    partNumber  :: NumberGr
-  , partTense   :: Tense
-  , partVoice   :: Voice
-  , partGender  :: Gender
-  , partCase    :: Case
-} deriving (Eq,Show)
+    partNumber  :: Maybe NumberGr
+  , partTense   :: Maybe Tense
+  , partVoice   :: Maybe Voice
+  , partGender  :: Maybe Gender
+  , partCase    :: Maybe Case
+} deriving (Eq,Show,Typeable)
 
 data Adjective = Adjective {
-    adjNumber  ::  NumberGr
-  , adjGender  ::  Gender
-  , adjCase    ::  Case
-  , adjDeg     ::  Degree
-} deriving (Eq,Show)
+    adjNumber  ::  Maybe NumberGr
+  , adjGender  ::  Maybe Gender
+  , adjCase    ::  Maybe Case
+  , adjDeg     ::  Maybe Degree
+} deriving (Eq,Show,Typeable)
 
 data Adverb = Adverb {
-    advDegree :: Degree
-} deriving (Eq,Show)
+    advDegree :: Maybe Degree
+} deriving (Eq,Show,Typeable)
 
 data Pronoun = Pronoun {
-    pronPerson  :: Person
-  , pronNumber  :: NumberGr
-  , pronGender  :: Gender
-  , pronCase    :: Case
-} deriving (Eq,Show)
+    pronPerson  :: Maybe Person
+  , pronNumber  :: Maybe NumberGr
+  , pronGender  :: Maybe Gender
+  , pronCase    :: Maybe Case
+} deriving (Eq,Show,Typeable)
 
 data Infinitive = Infinitive {
-    infTense :: Tense
-  , infVoice :: Voice
-} deriving (Eq,Show)
+    infTense :: Maybe Tense
+  , infVoice :: Maybe Voice
+} deriving (Eq,Show,Typeable)
 
 data Article = Article {
-    artNumber :: NumberGr
-  , artGender :: Gender
-  , artCase   :: Case
-} deriving (Eq,Show)
+    artNumber :: Maybe NumberGr
+  , artGender :: Maybe Gender
+  , artCase   :: Maybe Case
+} deriving (Eq,Show,Typeable)
 
 data Adverbial = Adverbial {
-  advlDegree :: Degree
-} deriving (Eq,Show)
+  advlDegree :: Maybe Degree
+} deriving (Eq,Show,Typeable)
 
-data Conjunction = Conjunction deriving (Eq,Show)
+data Conjunction = Conjunction deriving (Eq,Show,Typeable)
 
-data Particle = Particle deriving (Eq,Show)
+data Particle = Particle deriving (Eq,Show,Typeable)
 
 data Numeral = Numeral {
-    numlNumber :: NumberGr
-  , numlGender :: Gender
-  , numlCase   :: Case 
-} deriving (Eq,Show)
+    numlNumber :: Maybe NumberGr
+  , numlGender :: Maybe Gender
+  , numlCase   :: Maybe Case 
+} deriving (Eq,Show,Typeable)
 
-data Irregular = Irregular deriving (Eq,Show)
+data Irregular = Irregular deriving (Eq,Show,Typeable)
+
+type Key = T.Text
+type Value = MorphEntry
+
+data Database = Database !(M.Map Key [Value])
+  deriving (Show,Eq,Typeable)
+
+$(deriveSafeCopy 0 'base ''Case)
+$(deriveSafeCopy 0 'base ''Degree)
+$(deriveSafeCopy 0 'base ''NumberGr)
+$(deriveSafeCopy 0 'base ''Gender)
+$(deriveSafeCopy 0 'base ''Tense)
+$(deriveSafeCopy 0 'base ''Voice)
+$(deriveSafeCopy 0 'base ''Mood)
+$(deriveSafeCopy 0 'base ''Person)
+$(deriveSafeCopy 0 'base ''Adverb)
+$(deriveSafeCopy 0 'base ''Adverbial)
+$(deriveSafeCopy 0 'base ''Conjunction)
+$(deriveSafeCopy 0 'base ''Infinitive)
+$(deriveSafeCopy 0 'base ''Irregular)
+$(deriveSafeCopy 0 'base ''Numeral)
+$(deriveSafeCopy 0 'base ''Pronoun)
+$(deriveSafeCopy 0 'base ''Adjective)
+$(deriveSafeCopy 0 'base ''Noun)
+$(deriveSafeCopy 0 'base ''Exclamation)
+$(deriveSafeCopy 0 'base ''Preposition)
+$(deriveSafeCopy 0 'base ''Article)
+$(deriveSafeCopy 0 'base ''Participle)
+$(deriveSafeCopy 0 'base ''Particle)
+$(deriveSafeCopy 0 'base ''Verb)
+$(deriveSafeCopy 0 'base ''Morphology)
+$(deriveSafeCopy 0 'base ''MorphEntry)
+$(deriveSafeCopy 0 'base ''Database)
